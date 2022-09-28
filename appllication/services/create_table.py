60eb5618825c1f1348f448c3d0603907a67dfad5
Click1N <111238061+Click1N@users.noreply.github.com>
2022-09-28 23:24:39 +0200

    syntax  changes

--- appllication/services/create_table.py
+++ appllication/services/create_table.py
@@ -4,10 +4,7 @@ from appllication.services.db_connection import DBConnection
 def phone_table():
     with DBConnection() as connection:  # we creat connection and return it trough context manager
         with connection:  # context manager for connection for possible changes of the data
-            ## table creation , if it isn't exist'
-            # column1 datatype,
-            # column2 datatype,
-            # column3 datatype,
+            ## table creation , if it isn't exist' (column1 datatype),
             connection.execute(
                 """
                 CREATE TABLE IF NOT EXISTS phones ( 
