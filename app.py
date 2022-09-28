05e802f2b3549c291f1b4857d21ae3ad1cb93de3
Click1N <111238061+Click1N@users.noreply.github.com>
2022-09-28 15:43:39 +0200

    root creation for table creation function

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
 
+from appllication.services import create_table
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():  # put application's code here
     return "Hi World!"
+
+
+@app.route("/phone_book")
+def phones():  # put application's code here
+    return create_table()
 
 
 if __name__ == "__main__":
