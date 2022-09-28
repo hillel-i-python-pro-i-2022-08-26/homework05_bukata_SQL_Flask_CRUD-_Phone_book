a5ebb1a1f8bade6f9ce289eb8d76f0863cd6b3b1
Click1N <111238061+Click1N@users.noreply.github.com>
2022-09-28 15:42:50 +0200

    table creation function with connection for future database

---
+++ appllication/services/__init__.py
@@ -0,0 +1,2 @@
+from .create_table import create_table
+from .db_connection import DBConnection
---
+++ appllication/services/create_table.py
@@ -0,0 +1,19 @@
+from appllication.services import DBConnection
+
+
+def create_table():
+    with DBConnection() as connection:  # we creat connection and return it trough context manager
+        with connection:  # context manager for connection for possible changes of the data
+            ## table creation , if it isn't exist'
+            # column1 datatype,
+            # column2 datatype,
+            # column3 datatype,
+            connection.execute(
+                """
+                CREATE TABLE IF NOT EXISTS phones ( 
+                    PhoneID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    ContactName TEXT NOT NULL,
+                    PhoneValue INTEGER NOT NULL
+                )
+            """
+            )
